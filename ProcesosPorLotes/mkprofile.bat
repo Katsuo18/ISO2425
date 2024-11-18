35900119828d4e8585815943aaa7425bfc10d240
Katsuo18 <mmunozc40@iescastelar.com>
2024-11-18 09:44:13 +0100

    Add files via upload

--- ProcesosPorLotes/mkprofile.bat
+++ ProcesosPorLotes/mkprofile.bat
@@ -1,5 +1,7 @@
 @echo off
 Rem o: Este bat es para crear directorios en la carpeta de usuario
+Rem Autor: Miguel Muñoz Correa 
+Rem Fecha: 18/11/2024
 mkdir %USERPROFILE%\ASIR %USERPROFILE%\DAM %USERPROFILE%\SMR %USERPROFILE%\DAW %USERPROFILE%\CETI
 mkdir %USERPROFILE%\ASIR\docs pract exam %USERPROFILE%\DAM\docs pract exam %USERPROFILE%\SMR\docs pract exam
 mkdir %USERPROFILE%\DAW\docs pract exam %USERPROFILE%\CETI\docs pract exam
